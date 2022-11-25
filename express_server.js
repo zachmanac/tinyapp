75d508547c2704bd622a1d9718e565b1d2609ee8
zachmanac <zachstephenson94@gmail.com>
2022-11-25 13:40:25 -0800

    add new route for single URL and add template with url_show

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
