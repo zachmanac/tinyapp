1a19669d171a919517fc2bfffb75aa6e322c39e5
zachmanac <zachstephenson94@gmail.com>
2022-11-27 11:33:19 -0800

    add app.post for adding new links while generating shortURL for them, add new app.get to redirect to longURL

--- express_server.js
+++ express_server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+const generateRandomString = function () {   
+  return Math.random().toString(36).slice(2, 8); 
 };
 
+app.set("view engine", "ejs");
+
+const urlDatabase = {};
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls", (req, res) => {
+  
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -31,9 +35,28 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id].longURL
+  };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  const newURLID = generateRandomString();
+  const newURL = {
+    shortURL: newURLID,
+    longURL: req.body.longURL
+  };
+  urlDatabase[newURLID] = newURL;
+  res.redirect(`/urls/${newURLID}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id].longURL;
+  res.redirect(longURL);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
