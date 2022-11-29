15352785cfc8b39ecfaf5cb797c7bccfaf786873
zachmanac <zachstephenson94@gmail.com>
2022-11-28 17:18:14 -0800

    eslint express_server

--- express_server.js
+++ express_server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
-const generateRandomString = function () {   
-  return Math.random().toString(36).slice(2, 8); 
+const generateRandomString = function() {
+  return Math.random().toString(36).slice(2, 8);
 };
 
 app.set("view engine", "ejs");
@@ -15,25 +15,25 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.get("/urls", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     username: req.cookies["username"],
     urls: urlDatabase
-   };
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     username: req.cookies["username"],
-   };
+  };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     username: req.cookies["username"],
     id: req.params.id,
     shortURL: req.params.id,
