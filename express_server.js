95400bc0fe715edf88ce357bc3a051574a236cea
zachmanac <zachstephenson94@gmail.com>
2022-12-02 13:03:23 -0800

    add package cookie-session to replace cookie-parser, implement cookie-session routes

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const app = express();
 const PORT = 8080; // default port 8080
 const bcryptjs = require("bcryptjs");
 const salt = bcryptjs.genSaltSync(10);
+
 
 const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8);
@@ -56,14 +58,17 @@ const urlsForUser = function(id, urlDatabase) {
 };
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session_id',
+  keys: ['sample_session_key']
+}));
 
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
 app.get("/urls", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   if (!userID) {
     return res.status(401).send(`Please register and/or login to view page.\n`);
   }
@@ -75,7 +80,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   if(!userID) {
     return res.redirect("/login");
   }
@@ -86,7 +91,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const shortURL = req.params.id;
   const userURLS = urlsForUser(userID, urlDatabase);
   if(!userID) {
@@ -116,7 +121,7 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   if (userID) {
     return res.redirect("/urls");
   }
@@ -127,7 +132,7 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   if (userID) {
     return res.redirect("/urls");
   }
@@ -138,12 +143,12 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  if(!req.cookies["user_id"]) {
+  if(!req.session.user_id) {
     return res.status(401).send("Cannot shorten URLS without being logged in.\n");
   }
   const newURLID = generateRandomString();
   const newURL = {
-    userID: req.cookies["user_id"],
+    userID: req.session.user_id,
     longURL: req.body.longURL
   };
   urlDatabase[newURLID] = newURL;
@@ -151,7 +156,7 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const shortURL = req.params.id;
   const userURLS = urlsForUser(userID, urlDatabase);
   if(!urlDatabase[req.params.id]) {
@@ -173,7 +178,7 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const shortURL = req.params.id;
   const userURLS = urlsForUser(userID, urlDatabase);
   if(!urlDatabase[req.params.id]) {
@@ -196,19 +201,18 @@ app.post("/login", (req, res) => {
     return res.status(400).send('Both fields must be filled in to login.');
   }
   const user = getUserByEmail(users, req.body.email);
-  console.log("userpassword", user.password)
   if (!user) {
     return res.status(403).send(`Account with ${emailEntered} not found.`);
   }
   if (!bcryptjs.compareSync(passwordEntered, user.password)) {
     return res.status(403).send("Password entered is incorrect.");
   }
-  res.cookie('user_id', user.id);
+  req.session.user_id = user.id;
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect("/login");
 });
 
@@ -226,7 +230,7 @@ app.post("/register", (req, res) => {
     return res.status(400).send("Both fields must be filled in to register.");
   }
   users[newUserID] = newUser;
-  res.cookie('user_id', newUserID);
+  req.session.user_id = newUserID;
   res.redirect("/urls");
 });
 
