936b799f0453d89b541f66dc7d6c79ba609a7814
zachmanac <zachstephenson94@gmail.com>
2022-12-03 16:30:57 -0800

    eslint files

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,25 +4,25 @@ const { getUserByEmail, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail(testUsers, "user@example.com")
+    const user = getUserByEmail(testUsers, "user@example.com");
     const expectedUserID = "userRandomID";
     assert.deepEqual(user.id, expectedUserID);
   });
   it('should return undefined if non-existant email address', function() {
-    const user = getUserByEmail(testUsers, "usertestwrong@example.com")
+    const user = getUserByEmail(testUsers, "usertestwrong@example.com");
     const expectedUserID = undefined;
     assert.deepEqual(user, expectedUserID);
   });
@@ -41,17 +41,17 @@ const testurlDatabase = {
 
 describe('urlsForUser', function() {
   it('should return URLs for a given userID', function() {
-    const urls = urlsForUser("userRandomID", testurlDatabase)
+    const urls = urlsForUser("userRandomID", testurlDatabase);
     const expectedurls = {
-                          'b2xVn2': {
-                            userID: 'userRandomID',
-                            longURL: 'http://www.lighthouselabs.ca'
-                            }
-                          }
+      'b2xVn2': {
+        userID: 'userRandomID',
+        longURL: 'http://www.lighthouselabs.ca'
+      }
+    };
     assert.deepEqual(urls, expectedurls);
   });
   it('should return an empty object if userID has no URLs', function() {
-    const urls = urlsForUser("testID", testurlDatabase)
+    const urls = urlsForUser("testID", testurlDatabase);
     const expectedurls = {};
     assert.deepEqual(urls, expectedurls);
   });
